2b784c43ec70ae5c464e0f152a8e92ad1440b99e
Stephan Grootveld <sgrootveld@gmail.com>
2014-02-21 16:26:52 +0100

    Fixed RecursiveTransformer, added unittest

--- tests/FM/Feeder/Tests/Modifier/Item/Transformer/RecursiveTransformerTest.php
+++ tests/FM/Feeder/Tests/Modifier/Item/Transformer/RecursiveTransformerTest.php
@@ -3,24 +3,62 @@
 namespace FM\Feeder\Tests\Modifier\Item\Transformer;
 
 use FM\Feeder\Modifier\Data\Transformer\EmptyValueToNullTransformer;
-use FM\Feeder\Modifier\Item\Mapper\PathMapper;
-use FM\Feeder\Modifier\Item\Transformer\CallbackTransformer;
 use FM\Feeder\Modifier\Item\Transformer\RecursiveTransformer;
 use Symfony\Component\HttpFoundation\ParameterBag;
 
+/**
+ * Test recursive an array, EmptyValueToNullTransformer is used to test.
+ *
+ * Class RecursiveTransformerTest
+ * @package FM\Feeder\Tests\Modifier\Item\Transformer
+ */
 class RecursiveTransformerTest extends \PHPUnit_Framework_TestCase
 {
     public function testTransformer()
     {
         $item = new ParameterBag();
 
-        $item->add('key', 'value');
-        $item->add('array', ['value1', 'value2', '', ['value3', 'value4', '']]);
+        $item->set('key', 'value');
+        $item->set('array', ['value1', 'value2', '', ['value3', 'value4', '']]);
+
+        $original = $item->all();
 
         $transformer = new RecursiveTransformer(new EmptyValueToNullTransformer());
 
         $transformer->transform($item);
 
+        $array = $item->all();
 
+        $this->iterate($array, $original);
+
+    }
+
+    /**
+     * @param $resultValue
+     * @param $originalValue
+     */
+    protected function iterate($resultValue, $originalValue)
+    {
+        if (is_array($resultValue)) {
+            foreach($resultValue as $key=>$value){
+                $this->iterate($resultValue[$key], $originalValue[$key]);
+            }
+        } else {
+            $this->unittest($resultValue, $originalValue);
+        }
+    }
+
+    /**
+     * @param $resultValue
+     * @param $originalValue
+     */
+    protected function unittest($resultValue, $originalValue)
+    {
+        //An empty string should be transformed into an NULL value. So when the $originalValue is an empty string, I expect a NULL.
+        if ($originalValue === '') {
+            $originalValue = null;
+        }
+
+        $this->assertSame($resultValue, $originalValue);
     }
 }
